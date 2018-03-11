d12f767dc9b0f9ae0800010b86b5a9671e288e7b
BoykoNeov <boikoneov@gmail.com>
2018-03-12 01:20:21 +0200

    :diamond_shape_with_a_dot_inside: work on lcd_clock - implementing edditting through the buttons of the LCD shield

--- LCD/Clock/Clock.ino
+++ LCD/Clock/Clock.ino
@@ -7,12 +7,12 @@ const byte d5 = 5; //d5
 const byte d6 = 6; //d6
 const byte d7 = 7; //d7
 
-byte hours = 21;
-byte minutes = 55;
-byte seconds = 0;
+unsigned int hours = 0;
+unsigned int minutes = 0;
+unsigned int seconds = 0;
 
-byte month = 3;
-byte monthDay = 11;
+unsigned int month = 0;
+unsigned int monthDay = 1;
 unsigned int year = 2018;
 
 char* monthNames[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
@@ -22,20 +22,97 @@ unsigned long oldMillis = 0;
 unsigned long currentMillis = 0;
 bool isLeapYear = false;
 
+const byte blackChar[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+int blackCursorColumn = 15;
+int blackCursorRow = 0;
+int lastBlackCursorColumn = 15;
+int lastBlackCursorRow = 0;
+bool blackCursorActive = false;
+bool selectSwitch = false;
+bool editSwitch = false;
+int editActiveSecondsCounter = 0;
+
 LiquidCrystal lcd(resistorSelect, enable, d4, d5, d6, d7);
+
+#define btnRIGHT  1
+#define btnUP     2
+#define btnDOWN   4
+#define btnLEFT   3
+#define btnSELECT 5
+#define btnNONE   0
+
+byte oldPressedButton = 0;
+byte currentPressedButton = 0;
 
 void setup() 
 {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  lcd.createChar(0, blackChar);
+  
   CheckForLeapYear();
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  // set the cursor to column 0, line 1
+   if (editSwitch)
+   {
+     lcd.setCursor(12,0);
+     lcd.print("EDIT");
+   }
+   else
+   {
+    lcd.setCursor(12,0);
+    lcd.print("    ");
+   }
 
-lcd.setCursor(0,0);
+  
+  currentPressedButton = read_LCD_buttons();
+
+  if (currentPressedButton != oldPressedButton)
+  {
+    oldPressedButton = currentPressedButton;
+
+    if (!editSwitch)
+    {
+       MoveSelector(currentPressedButton);
+    }
+    else
+    {
+      EditDateTime(currentPressedButton);
+    }
+  }
+
+if (selectSwitch)
+{
+  ClearLastBlackCursor();
+ lastBlackCursorColumn = blackCursorColumn;
+ lastBlackCursorRow = blackCursorRow;
+
+
+  lcd.setCursor(blackCursorColumn, blackCursorRow);
+
+  if (seconds % 2 == 0)
+  {
+      lcd.write((byte)0);
+  }
+  else
+  {
+    lcd.print(' ');
+  }
+}
+  
+  lcd.setCursor(0,0);
   lcd.print(year);
   lcd.print("/");
   lcd.print(monthNames[month]);
@@ -57,6 +134,12 @@ lcd.setCursor(0,0);
  
  lcd.print(hours);
  lcd.print(':');
+
+  if(minutes < 10)
+  {
+    lcd.print('0');
+  }
+ 
  lcd.print(minutes);
  lcd.print(':');
 
@@ -66,11 +149,9 @@ lcd.setCursor(0,0);
  }
  
  lcd.print(seconds);
- 
-  
-  // print the number of milis since reset:
- // lcd.print(millis());
-  lcd.print(" ");
+
+  // prints the character defined as 0
+  // lcd.write((byte)0);
 
   //Update time
   currentMillis = millis();
@@ -79,6 +160,18 @@ lcd.setCursor(0,0);
   {
     seconds++;
     oldMillis += 1000;
+
+    if (editSwitch)
+    {
+         editActiveSecondsCounter++; 
+    }
+  }
+
+//Deactivate edit switch after 20 seconds
+  if (editActiveSecondsCounter >= 20)
+  {
+    editActiveSecondsCounter = 0;
+    editSwitch = false;
   }
 
   if (seconds > 59)
@@ -103,13 +196,165 @@ lcd.setCursor(0,0);
   {
     month++;
     monthDay = 1;
+  }
 
-    if (month > 12)
+  if (month > 11)
+   {
+    year++;
+    month = 0;
+    CheckForLeapYear();
+   }
+}
+
+void EditDateTime(byte pressedButtonNumber)
+{
+  int changingValue;
+
+  if (blackCursorRow == 0)
+  {
+    if (blackCursorColumn <= 4)
     {
-      year++;
-      month = 1;
-      CheckForLeapYear();
+     ChangeValue(year, pressedButtonNumber);
     }
+    else if (blackCursorColumn <= 8)
+    {
+      ChangeValue(month, pressedButtonNumber);
+    }
+    else if (blackCursorColumn <= 10)
+    {
+      ChangeValue(monthDay, pressedButtonNumber);
+    }
+    else
+    {
+      return;
+    }
+  }
+  else if (blackCursorRow == 1)
+  {
+    if (blackCursorColumn <= 2)
+    {
+      ChangeValue(hours, pressedButtonNumber);
+    }
+    else if (blackCursorColumn <= 5)
+    {
+      ChangeValue(minutes, pressedButtonNumber);
+    }
+    else if (blackCursorColumn <= 7)
+    {
+      ChangeValue(seconds, pressedButtonNumber);
+    }
+    else
+    {
+      return;
+    }
+  }
+  else
+  {
+    return;
+  }
+}
+
+void ChangeValue(unsigned int &value, byte pressedButton)
+{
+    if (pressedButton == 0)
+  {
+    return;
+  }
+  else if(pressedButton == 1) //right
+  {
+    return;
+  }
+  else if(pressedButton == 2) // up
+  {
+    value++;
+  }
+  else if(pressedButton == 3) // left
+  {
+    return;
+  }
+  else if(pressedButton == 4) // down
+  {
+    value--;
+  }
+  else if(pressedButton == 5) // select
+  {   
+    selectSwitch = true;
+    editSwitch = false;
+  }
+}
+
+void MoveSelector(byte pressedButtonNumber)
+{
+  if (pressedButtonNumber == 0)
+  {
+    return;
+  }
+  else if(pressedButtonNumber == 1) //right
+  {
+    blackCursorColumn++;
+    FixBlackCursorColumn();   
+  }
+  else if(pressedButtonNumber == 2) // up
+  {
+    blackCursorRow--;
+    FixBlackCursorRow();
+  }
+  else if(pressedButtonNumber == 3) // left
+  {
+    blackCursorColumn--;
+    FixBlackCursorColumn();   
+  }
+  else if(pressedButtonNumber == 4) // down
+  {
+    blackCursorRow++;
+    FixBlackCursorRow();
+  }
+  else if(pressedButtonNumber == 5) // select
+  {   
+    selectSwitch = !selectSwitch;
+
+    if (!selectSwitch)
+    {
+      ClearLastBlackCursor();
+      editSwitch = !editSwitch;
+    }
+    else
+    {
+      editSwitch = false;
+    }
+  }
+}
+
+ void ClearLastBlackCursor()
+ {
+    lcd.setCursor(lastBlackCursorColumn, lastBlackCursorRow);
+    lcd.print(' ');
+ }
+
+
+void FixBlackCursorColumn()
+{  
+  if (blackCursorColumn < 0)
+  {
+     blackCursorColumn = 15;
+  }
+
+  if (blackCursorColumn > 15)
+  {
+    blackCursorColumn = 0;
+  }
+}
+
+void FixBlackCursorRow()
+{  
+  if (blackCursorRow < 0)
+  {
+     blackCursorRow = 1;
+  }
+
+  if (blackCursorRow > 1)
+  {
+    blackCursorRow = 0;
   }
 }
 
@@ -140,4 +385,46 @@ void CheckForLeapYear()
   {
     monthLengths[1] = 29;
   }
+  else
+  {
+    monthLengths[1] = 28;
+  }
+}
+
+// read the buttons
+byte read_LCD_buttons()
+{
+ int adc_key_in = analogRead(0);
+ 
+ if (adc_key_in > 1000)
+ {
+  return btnNONE;
+ }
+
+ if (adc_key_in < 50)  // approx 0
+ {
+  return btnRIGHT;
+ }
+
+ if (adc_key_in < 250)  //aprox 141, 142
+ {
+   return btnUP;
+ }
+ 
+ if (adc_key_in < 450) //aprox 326
+{
+   return btnDOWN;
+}
+ 
+ if (adc_key_in < 650)   //approx 501, 502
+ {
+   return btnLEFT;
+ }
+ 
+ if (adc_key_in < 850)   //aprox 739/740
+ {
+  return btnSELECT;
+ }
+
+ return btnNONE;  // when all others fail, return this... 
 }

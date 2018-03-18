c107a568154a1b5ff72e14382edc024d47111aaf
BoykoNeov <boikoneov@gmail.com>
2018-03-19 00:47:56 +0200

    :red_car: assembly function , equivalent to Arduino's 'constrain' function, accepts unsigned ints

---
+++ Assembly/unsigned_int_ConstrainFunction/unsigned_int_ConstrainFunction.ino
@@ -0,0 +1,11 @@
+extern "C" {
+  // function prototypes
+  unsigned int ConstrainAssembly(unsigned int number, unsigned int lowerLimit, unsigned int upperLimit);
+}
+
+void setup() {
+}
+
+void loop() 
+{
+}

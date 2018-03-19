2e8c2de646ad5f7b6e32f1c9285b4a27c757af79
BoykoNeov <boikoneov@gmail.com>
2018-03-19 11:56:52 +0200

    :ship: added info about variable and function declaration in assembly ; added an example for using a global variable in assembly

---
+++ Assembly/example_using_a_global_variable_read_parse_string_from_serial/example_using_a_global_variable_read_parse_string_from_serial.ino
@@ -0,0 +1,67 @@
+extern "C" {
+  long DecrementBy1();
+}
+
+ long longvar = 0;
+ 
+char inData[20]; // Allocate some space for the string
+char inChar = 1; // Where to store the character read
+byte index = 0; // Index into array; where to store the character
+
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+  static boolean needPrompt = true;
+  char* userInput;
+  
+  if (needPrompt)
+  {
+    Serial.print("Please enter inputs and press enter at the end:\n");
+    needPrompt=false;
+  }
+  
+  userInput= serialString();
+  if (userInput!=NULL)
+  {
+    Serial.print("You entered: ");
+    Serial.println(userInput);
+    longvar = atol(userInput);
+    longvar = DecrementBy1();
+    Serial.print("result: ");
+    Serial.println(longvar);
+    Serial.println();
+    needPrompt=true;
+  }
+}
+
+char* serialString()
+{
+  static char str[21]; // For strings of max length=20
+  
+  if (!Serial.available())
+  {
+    return NULL;
+  }
+  
+  delay(64); // wait for all characters to arrive
+  memset(str,0,sizeof(str)); // clear str
+  byte count=0;
+  
+  while (Serial.available())
+  {
+    char c=Serial.read();
+    if (c>=32 && count<sizeof(str)-1)
+    {
+      str[count]=c;
+      count++;
+    }
+  }
+  
+  str[count]='\0'; // make it a zero terminated string
+  return str;
+}
+

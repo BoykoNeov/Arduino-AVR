597799447148caac46419422347ca8f9f214a134
BoykoNeov <boikoneov@gmail.com>
2018-03-25 01:03:07 +0200

    :banana: implemented two functions in assembly, which are similiar to delay(), but take number of cycles (times 4 and times 6 respectively) for which to delay

---
+++ Assembly/DelayMeasuredInCycles/DelayMeasuredInCycles.ino
@@ -0,0 +1,15 @@
+extern "C" 
+{
+    // Takes as input (uint16) x cycles and returns after (x times 4 cycles) + around 9 , 0 is MaxValue;
+  void DelayCyclesX6(uint32_t cyclesX6);
+  
+    // Takes as input (uint32) x cycles and returns after (x times 6 cycles) + around 11;
+  void DelayCyclesX4(uint16_t cyclesX4);
+}
+
+void setup() {
+}
+
+void loop() 
+{
+}

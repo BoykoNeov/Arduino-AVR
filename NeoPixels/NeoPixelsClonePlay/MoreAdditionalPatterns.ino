752fac116e2579b99ac9913abc24936da88d9068
BoykoNeov <boikoneov@gmail.com>
2018-10-06 23:05:53 +0300

    :flashlight: added rearrangementfor light and dark pixels

--- NeoPixels/NeoPixelsClonePlay/MoreAdditionalPatterns.ino
+++ NeoPixels/NeoPixelsClonePlay/MoreAdditionalPatterns.ino
@@ -1,76 +1,201 @@
 // Sorting Colors is not a trivial task
 void BubbleSortColorsByAbsoluteValue(uint32_t wait)
 {
-      uint32_t millisAtStart = millis();
-      uint32_t currentMillis = millisAtStart;
+	uint32_t millisAtStart = millis();
 
-      bool anythingChanged = true;
-      while(anythingChanged)
-      {
-        anythingChanged = false;
+	bool anythingChanged = true;
+	while (anythingChanged)
+	{
+		anythingChanged = false;
 
-        for (uint8_t p1 = 0; p1 < strip.numPixels() - 1; p1++)
-        {
-            long lngRGB = strip.getPixelColor(p1);
-            long lngRGBnext = strip.getPixelColor(p1 + 1);
-            if (lngRGBnext > lngRGB)
-            {
-              strip.setPixelColor(p1, lngRGBnext);
-              strip.setPixelColor(p1 + 1, lngRGB);
-              strip.show();
-              anythingChanged = true;
-            }
-        }
-      }
+		for (uint8_t p1 = 0; p1 < strip.numPixels() - 1; p1++)
+		{
+			long lngRGB = strip.getPixelColor(p1);
+			long lngRGBnext = strip.getPixelColor(p1 + 1);
+			if (lngRGBnext > lngRGB)
+			{
+				strip.setPixelColor(p1, lngRGBnext);
+				strip.setPixelColor(p1 + 1, lngRGB);
+				strip.show();
+				anythingChanged = true;
+			}
+		}
+	}
 
-      while (millisAtStart + wait > millis())
-      {}
+	while (millisAtStart + wait > millis())
+	{
+	}
 }
 
 void FlashingAllPixels(uint8_t startingMillisecondsWait, uint8_t endingMillisecondsWait, uint16_t milisecondsToCycleEachValue, uint8_t red, uint8_t green, uint8_t blue)
 {
-   uint32_t millisAtStart = millis();
-   uint32_t currentMillis = millisAtStart;
-  
-  if (startingMillisecondsWait == endingMillisecondsWait)
-  {
-    while (millisAtStart + milisecondsToCycleEachValue > millis())
-    {
-      stripSet(red, green, blue, startingMillisecondsWait);
-      stripSet(0,0,0,startingMillisecondsWait);
-    } 
-  }
-  
-  while (startingMillisecondsWait < endingMillisecondsWait)
-  {
-    while (millisAtStart + milisecondsToCycleEachValue > millis())
-    {
-      stripSet(red, green, blue, startingMillisecondsWait);
-      stripSet(0,0,0,startingMillisecondsWait);
-    } 
+	uint32_t millisAtStart = millis();
 
-    startingMillisecondsWait++;
-    millisAtStart = millis();
-  }
+	if (startingMillisecondsWait == endingMillisecondsWait)
+	{
+		while (millisAtStart + milisecondsToCycleEachValue > millis())
+		{
+			stripSet(red, green, blue, startingMillisecondsWait);
+			stripSet(0, 0, 0, startingMillisecondsWait);
+		}
+	}
 
-   while (startingMillisecondsWait > endingMillisecondsWait)
-  {
-    while (millisAtStart + milisecondsToCycleEachValue > millis())
-    {
-      stripSet(red, green, blue, startingMillisecondsWait);
-      stripSet(0,0,0,startingMillisecondsWait);
-    } 
+	while (startingMillisecondsWait < endingMillisecondsWait)
+	{
+		while (millisAtStart + milisecondsToCycleEachValue > millis())
+		{
+			stripSet(red, green, blue, startingMillisecondsWait);
+			stripSet(0, 0, 0, startingMillisecondsWait);
+		}
 
-    startingMillisecondsWait--;
-    millisAtStart = millis();
-  }
+		startingMillisecondsWait++;
+		millisAtStart = millis();
+	}
+
+	while (startingMillisecondsWait > endingMillisecondsWait)
+	{
+		while (millisAtStart + milisecondsToCycleEachValue > millis())
+		{
+			stripSet(red, green, blue, startingMillisecondsWait);
+			stripSet(0, 0, 0, startingMillisecondsWait);
+		}
+
+		startingMillisecondsWait--;
+		millisAtStart = millis();
+	}
 }
 
 void FlashingAllPixels(uint8_t startingMillisecondsWait, uint8_t endingMillisecondsWait, uint16_t timesToCycleEachValue, int32_t color)
 {
-           uint8_t red = (uint8_t)((color >> 16) & 0xff);
-           uint8_t green  = (uint8_t)((color >> 8) & 0xff);
-           uint8_t blue = (uint8_t)(color & 0xff);
+	uint8_t red = (uint8_t)((color >> 16) & 0xff);
+	uint8_t green = (uint8_t)((color >> 8) & 0xff);
+	uint8_t blue = (uint8_t)(color & 0xff);
 
-           FlashingAllPixels(startingMillisecondsWait, endingMillisecondsWait, timesToCycleEachValue, red, green, blue);
+	FlashingAllPixels(startingMillisecondsWait, endingMillisecondsWait, timesToCycleEachValue, red, green, blue);
+}
+
+void SequentalRandomChange(uint32_t wait, uint32_t delayBetweenPixelsInMicros)
+{
+	uint32_t millisAtStart = millis();
+
+	while (millisAtStart + wait > millis()) 
+	{
+		for (size_t r = 0; r < strip.numPixels(); r++)
+		{
+			strip.setPixelColor(r, random(0, 256), random(0, 256), random(0, 256));
+			strip.show();
+			delayMicroseconds(delayBetweenPixelsInMicros);
+		}
+	}
+}
+
+void RandomAndDarkPixels(uint8_t chanceOfDarkPixels) 
+{
+	for (size_t i = 0; i < strip.numPixels(); i++)
+	{
+		bool dark = false;
+		uint8_t rnd = random(0, 101);
+
+		if (rnd < chanceOfDarkPixels) 
+		{
+			dark = true;
+		}
+
+		if (dark) 
+		{
+			strip.setPixelColor(i, 0, 0, 0);
+		}
+		else if (random(0, 10) < 4) 
+		{
+			strip.setPixelColor(i, random(0, 2) * 255, random(0, 2) * 255, random(0, 2) * 255);
+		}
+		else if (random(0, 2) == 0)
+		{
+			strip.setPixelColor(i, random(0, 2) * 255, random(0, 2) * 255, 0);
+		}
+		else 
+		{
+			strip.setPixelColor(i, random(0, 256), random(0, 256), random(0, 256));
+		}
+	}
+}
+
+void RearrangeDarkPixelsSequentally(uint16_t waitBetweenRearrangements) 
+{
+	byte initialBrightness = strip.getBrightness();
+	bool anythingChanged = true;
+	while (anythingChanged)
+	{
+		anythingChanged = false;
+		for (size_t ic = 0; ic < NumberOfPixels - 1; ic++)
+		{
+			bool changeThisCycle = false;
+			// without this setBrightness nonsense, some pixels disappear weight brightness is low
+			strip.setBrightness(255);
+			long lngRGB = strip.getPixelColor(ic);
+			long lngRGBnext = strip.getPixelColor(ic + 1);
+
+			if (lngRGBnext == 0 && lngRGB != 0)
+			{
+				strip.setPixelColor(ic, lngRGBnext);
+				strip.setPixelColor(ic + 1, lngRGB);
+				strip.setBrightness(initialBrightness);
+				strip.show();
+				anythingChanged = true;
+				changeThisCycle = true;
+			}		
+
+			if (changeThisCycle) 
+			{
+				delay(waitBetweenRearrangements);
+			}
+			else 
+			{
+				delay(waitBetweenRearrangements / 3);
+			}
+		}
+	}
+
+	strip.setBrightness(initialBrightness);
+	strip.show();
+}
+
+void RearrangeDarkPixelsSequentallyLightFirst(uint16_t waitBetweenRearrangements)
+{
+	byte initialBrightness = strip.getBrightness();
+	bool anythingChanged = true;
+	while (anythingChanged)
+	{
+		anythingChanged = false;
+		for (size_t ic = NumberOfPixels - 1; ic > 0; ic--)
+		{
+			bool changeThisCycle = false;
+			// without this setBrightness nonsense, some pixels disappear weight brightness is low
+			strip.setBrightness(255);
+			long lngRGB = strip.getPixelColor(ic);
+			long lngRGBnext = strip.getPixelColor(ic - 1);
+
+			if (lngRGBnext == 0 && lngRGB != 0)
+			{
+				strip.setPixelColor(ic, lngRGBnext);
+				strip.setPixelColor(ic - 1, lngRGB);
+				strip.setBrightness(initialBrightness);
+				strip.show();
+				anythingChanged = true;
+				changeThisCycle = true;
+			}
+
+			if (changeThisCycle)
+			{
+				delay(waitBetweenRearrangements);
+			}
+			else
+			{
+				delay(waitBetweenRearrangements / 3);
+			}
+		}
+	}
+
+	strip.setBrightness(initialBrightness);
+	strip.show();
 }
--- NeoPixels/NeoPixelsClonePlay/NeoPixelsClonePlay.ino
+++ NeoPixels/NeoPixelsClonePlay/NeoPixelsClonePlay.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 #define PIN 6
@@ -22,273 +22,292 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NumberOfPixels, PIN, NEO_GRB + NEO_K
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+	strip.begin();
+	strip.show(); // Initialize all pixels to 'off'
+	strip.setBrightness(40);
 
-  FlashingAllPixels(10, 50, 31, 255, 255, 255);
-  FlashingAllPixels(50, 10, 31, 255, 255, 255);
+	RandomAndDarkPixels(50);
+	delay(1000);
+	RearrangeDarkPixelsSequentally(50);
+	RearrangeDarkPixelsSequentallyLightFirst(50);
+	delay(1000);
 
- twinkleRand(40 ,strip.Color(random(0,255),random(0,255),random(0,255)),strip.Color(random(0,255), random(0, 255), random(0,255)),90); 
+	RandomAndDarkPixels(50);
+	RearrangeDarkPixelsSequentallyLightFirst(50);
+	RearrangeDarkPixelsSequentally(50);
+	delay(1000);
 
-GradualRandomChange(1000);
-delay (10000);
 
-WarmRandomSwitching(1000);
-BubbleSortColorsByAbsoluteValue(1000);
+	for (size_t i1 = 0; i1 < 2; i1++) 
+	{
+		SequentalRandomChange(5000, 0);
+		SequentalRandomChange(5000, 10000);
+	}
 
-delay(1000);
+
+
+	FlashingAllPixels(10, 50, 31, 255, 255, 255);
+	FlashingAllPixels(50, 10, 31, 255, 255, 255);
+
+	twinkleRand(40, strip.Color(random(0, 255), random(0, 255), random(0, 255)), strip.Color(random(0, 255), random(0, 255), random(0, 255)), 90);
+
+	GradualRandomChange(1000);
+	delay(10000);
+
+	WarmRandomSwitching(1000);
+	BubbleSortColorsByAbsoluteValue(1000);
+
+	delay(1000);
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  
-//  colorWipe(strip.Color(255, 0, 0), 100); // Red
-//  colorWipe(strip.Color(0, 255, 0), 100); // Green
-//  colorWipe(strip.Color(0, 0, 255), 100); // Blue
-//  colorWipe(strip.Color(0, 0, 0, 255), 100); // White RGBW
-//  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 100); // White
-//  theaterChase(strip.Color(127, 0, 0), 100); // Red
-//  theaterChase(strip.Color(0, 0, 127), 100); // Blue
-//
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
-  WarmRandomSwitching(1000);
-  FlashingAllPixels(20, 50, 30, 255, 255, 255);
-  FlashingAllPixels(50, 20, 30, 255, 255, 255);
-  theaterChaseRainbow(50);
-  rainbowCycle(20);
+	// Some example procedures showing how to display to the pixels:
+
+  //  colorWipe(strip.Color(255, 0, 0), 100); // Red
+  //  colorWipe(strip.Color(0, 255, 0), 100); // Green
+  //  colorWipe(strip.Color(0, 0, 255), 100); // Blue
+  //  colorWipe(strip.Color(0, 0, 0, 255), 100); // White RGBW
+  //  // Send a theater pixel chase in...
+	theaterChase(strip.Color(127, 127, 127), 100); // White
+  //  theaterChase(strip.Color(127, 0, 0), 100); // Red
+  //  theaterChase(strip.Color(0, 0, 127), 100); // Blue
+  //
+	rainbow(20);
+	rainbowCycle(20);
+	theaterChaseRainbow(50);
+	WarmRandomSwitching(1000);
+	FlashingAllPixels(20, 50, 30, 255, 255, 255);
+	FlashingAllPixels(50, 20, 30, 255, 255, 255);
+	theaterChaseRainbow(50);
+	rainbowCycle(20);
 }
 
 // number, twinkle color, background color, delay
 // twinkleRand(5,strip.Color(255,255,255),strip.Color(255, 0, 100),90);
 
 // twinkle random number of pixels
-void twinkleRand(int num,uint32_t c,uint32_t bg,int wait) {
-  // set background
-   stripSet(bg,0);
-   // for each num
-   for (int i=0; i<num; i++) {
-     strip.setPixelColor(random(strip.numPixels()),c);
-   }
-  strip.show();
-  delay(wait);
+void twinkleRand(int num, uint32_t c, uint32_t bg, int wait) {
+	// set background
+	stripSet(bg, 0);
+	// for each num
+	for (int i = 0; i < num; i++) {
+		strip.setPixelColor(random(strip.numPixels()), c);
+	}
+	strip.show();
+	delay(wait);
 }
 
- // very simple wave: velocity, cycles,delay between frames
- // simpleWave(0.03,5,10);
-void simpleWave(float rate,int cycles, int wait) {
-   float pos=0.0;
-  // cycle through x times
-  for(int x=0; x<(strip.numPixels()*cycles); x++)
-  {
-      pos = pos+rate;
-      for(int i=0; i<strip.numPixels(); i++) {
-        // sine wave, 3 offset waves make a rainbow!
-        float level = sin(i+pos) * 127 + 128;
-        // set color level 
-        strip.setPixelColor(i,(int)level,0,0);
-      }
-         strip.show();
-         delay(wait);
-  }
+// very simple wave: velocity, cycles,delay between frames
+// simpleWave(0.03,5,10);
+void simpleWave(float rate, int cycles, int wait) {
+	float pos = 0.0;
+	// cycle through x times
+	for (int x = 0; x < (strip.numPixels()*cycles); x++)
+	{
+		pos = pos + rate;
+		for (int i = 0; i < strip.numPixels(); i++) {
+			// sine wave, 3 offset waves make a rainbow!
+			float level = sin(i + pos) * 127 + 128;
+			// set color level 
+			strip.setPixelColor(i, (int)level, 0, 0);
+		}
+		strip.show();
+		delay(wait);
+	}
 }
 
 void GradualRandomChange(uint32_t wait)
-    {
-      uint8_t targetColor[120];
-      for (uint8_t k = 0; k < 120; k++)
-      {
-        targetColor[k] = random(0, 20);
-      }
-  
-      uint32_t millisAtStart = millis();
-      uint32_t currentMillis = millisAtStart;
+{
+	uint8_t targetColor[120];
+	for (uint8_t k = 0; k < 120; k++)
+	{
+		targetColor[k] = random(0, 20);
+	}
 
-      while (millisAtStart + wait > millis())
-      {
-        for (uint8_t k = 0; k < strip.numPixels(); k++)
-        {
-        uint8_t c[3]; 
-        long lngRGB = strip.getPixelColor(k);
-        c[0] = (uint8_t)((lngRGB >> 16) & 0xff);
-        c[1] = (uint8_t)((lngRGB >> 8) & 0xff);
-        c[2] = (uint8_t)(lngRGB & 0xff);
+	uint32_t millisAtStart = millis();
 
-          for (uint8_t j = 0; j < 3; j++)
-          {
-            if (c[j] == targetColor[j + k * 3])
-            {
-              if (j == 2)
-              {
-                targetColor[j + k * 3] = random(0, 40);
-              }
-              else if (j == 1)
-              {
-                targetColor[j + k * 3] = random(0, 117);
-              }
-              else
-              {
-                targetColor[j + k * 3] = random(0, 256);
-              }
-            }
-            else if (c[j] > targetColor[j + k * 3])
-            {
-              c[j]--;
-            }
-            else
-            {
-              c[j]++;
-            }
-          }
+	while (millisAtStart + wait > millis())
+	{
+		for (uint8_t k = 0; k < strip.numPixels(); k++)
+		{
+			uint8_t c[3];
+			long lngRGB = strip.getPixelColor(k);
+			c[0] = (uint8_t)((lngRGB >> 16) & 0xff);
+			c[1] = (uint8_t)((lngRGB >> 8) & 0xff);
+			c[2] = (uint8_t)(lngRGB & 0xff);
 
-        strip.setPixelColor(k, c[0], c[1], c[2]);
-        strip.show();
-        }
-      }
-    }
+			for (uint8_t j = 0; j < 3; j++)
+			{
+				if (c[j] == targetColor[j + k * 3])
+				{
+					if (j == 2)
+					{
+						targetColor[j + k * 3] = random(0, 40);
+					}
+					else if (j == 1)
+					{
+						targetColor[j + k * 3] = random(0, 117);
+					}
+					else
+					{
+						targetColor[j + k * 3] = random(0, 256);
+					}
+				}
+				else if (c[j] > targetColor[j + k * 3])
+				{
+					c[j]--;
+				}
+				else
+				{
+					c[j]++;
+				}
+			}
 
-void WarmRandomSwitching(uint32_t wait){
-  for (uint8_t i = 0; i < strip.numPixels(); i++)
-    {
-      strip.setPixelColor(i, (uint8_t)random(0,256), (uint8_t)random(0, 256), (uint8_t)random(0,256));
-      strip.show();
-    }
-  
-   uint32_t millisAtStart = millis();
-   uint32_t currentMillis = millisAtStart;
+			strip.setPixelColor(k, c[0], c[1], c[2]);
+			strip.show();
+		}
+	}
+}
 
-   while (millisAtStart + wait > millis())
-   {
-    for (uint8_t i = 0; i < strip.numPixels(); i++)
-    {
-      uint8_t c[3]; 
-      long lngRGB = strip.getPixelColor(i);
-      c[0] = (uint8_t)((lngRGB >> 16) & 0xff),
-      c[1] = (uint8_t)((lngRGB >> 8) & 0xff),
-      c[2] = (uint8_t)(lngRGB & 0xff);
+void WarmRandomSwitching(uint32_t wait) {
+	for (uint8_t i = 0; i < strip.numPixels(); i++)
+	{
+		strip.setPixelColor(i, (uint8_t)random(0, 256), (uint8_t)random(0, 256), (uint8_t)random(0, 256));
+		strip.show();
+	}
 
-      for (uint8_t j = 0; j < 3; j++)
-      {
-        c[j] += random(-20,21);
-      }
+	uint32_t millisAtStart = millis();
 
-      if (c[2] >= 80)
-      {
-        c[2] -= 80;
-      }
+	while (millisAtStart + wait > millis())
+	{
+		for (uint8_t i = 0; i < strip.numPixels(); i++)
+		{
+			uint8_t c[3];
+			long lngRGB = strip.getPixelColor(i);
+			c[0] = (uint8_t)((lngRGB >> 16) & 0xff),
+				c[1] = (uint8_t)((lngRGB >> 8) & 0xff),
+				c[2] = (uint8_t)(lngRGB & 0xff);
 
-      strip.setPixelColor(i, c[0], c[1], c[2]);
-      strip.show();
-    }
-   }
+			for (uint8_t j = 0; j < 3; j++)
+			{
+				c[j] += random(-20, 21);
+			}
+
+			if (c[2] >= 80)
+			{
+				c[2] -= 80;
+			}
+
+			strip.setPixelColor(i, c[0], c[1], c[2]);
+			strip.show();
+		}
+	}
 }
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
+	for (uint16_t i = 0; i < strip.numPixels(); i++) {
+		strip.setPixelColor(i, c);
+		strip.show();
+		delay(wait);
+	}
 }
 
 void rainbow(uint8_t wait) {
-  uint16_t i, j;
+	uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
+	for (j = 0; j < 256; j++) {
+		for (i = 0; i < strip.numPixels(); i++) {
+			strip.setPixelColor(i, Wheel((i + j) & 255));
+		}
+		strip.show();
+		delay(wait);
+	}
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
+	uint16_t i, j;
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
+	for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+		for (i = 0; i < strip.numPixels(); i++) {
+			strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+		}
+		strip.show();
+		delay(wait);
+	}
 }
 
 //Theatre-style crawling lights.
 void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
+	for (int j = 0; j < 10; j++) {  //do 10 cycles of chasing
+		for (int q = 0; q < 3; q++) {
+			for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+				strip.setPixelColor(i + q, c);    //turn every third pixel on
+			}
+			strip.show();
 
-      delay(wait);
+			delay(wait);
 
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
+			for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+				strip.setPixelColor(i + q, 0);        //turn every third pixel off
+			}
+		}
+	}
 }
 
 //Theatre-style crawling lights with rainbow effect
 void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
+	for (int j = 0; j < 256; j++) {     // cycle all 256 colors in the wheel
+		for (int q = 0; q < 3; q++) {
+			for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+				strip.setPixelColor(i + q, Wheel((i + j) % 255));    //turn every third pixel on
+			}
+			strip.show();
 
-      delay(wait);
+			delay(wait);
 
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
+			for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+				strip.setPixelColor(i + q, 0);        //turn every third pixel off
+			}
+		}
+	}
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+	WheelPos = 255 - WheelPos;
+	if (WheelPos < 85) {
+		return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+	}
+	if (WheelPos < 170) {
+		WheelPos -= 85;
+		return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+	}
+	WheelPos -= 170;
+	return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 // quickly set the entire strip a color
 void stripSet(uint8_t red, uint8_t green, uint8_t blue, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) 
-  {
-      strip.setPixelColor(i, red, green, blue);
-  }
-  // move the show outside the loop
-  strip.show();
-  delay(wait);
+	for (uint16_t i = 0; i < strip.numPixels(); i++)
+	{
+		strip.setPixelColor(i, red, green, blue);
+	}
+	// move the show outside the loop
+	strip.show();
+	delay(wait);
 }
 
 // quickly set the entire strip a color
-void stripSet(uint32_t c, uint8_t wait) 
+void stripSet(uint32_t c, uint8_t wait)
 {
-  for(uint16_t i=0; i<strip.numPixels(); i++) 
-  {
-      strip.setPixelColor(i, c);
-  }
-  // move the show outside the loop
-  strip.show();
-  delay(wait);
+	for (uint16_t i = 0; i < strip.numPixels(); i++)
+	{
+		strip.setPixelColor(i, c);
+	}
+	// move the show outside the loop
+	strip.show();
+	delay(wait);
 }
